a06d34f4e082e3a5d3ddee09d9f1b6f89f8015db
RobertAndrei99 <86976537+RobertAndrei99@users.noreply.github.com>
2021-10-18 17:59:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,106 +1,106 @@
 document.body.style.background = "url('background.jpg') " ;
-const animale = ["BRONTOZAUR" , "CAINE" , "RAC" , "PIROPOPIRCANITA"] ;
-const mancare = ["BRANZOAICE" , "CALTABOS" , "COLIVA" , "COZONAC"] ;
-const categorii = ["Animale" , "Mancare"] ;
-function ascundeVieti() {
-  document.getElementById("Vieti").style.display = "none";
-  document.getElementById("categorieAleasa").style.display = "none";
+const animals = ["HIPPOPOTAMUS" , "DOG" , "BUFFALO" , "CROCODILE"] ;
+const food = ["SPAGHETTI" , "PIZZA" , "HAMBURGER" , "JALAPENO" ] ;
+const categories = ["Animals" , "Food"] ;
+function hideLifes() {
+  document.getElementById("lifes").style.display = "none";
+  document.getElementById("categoryChosen").style.display = "none";
 }
-function ascundeJoc() {
-  document.getElementById("joc").style.display = "none";
+function hideGame() {
+  document.getElementById("game").style.display = "none";
 }
-function ascundeLitera(idButon) {
-  document.getElementById(idButon).style.display = "none";
+function hideLetter(idButton) {
+  document.getElementById(idButton).style.display = "none";
 }
-function ascundeCategorie(idCategorie){
-  document.getElementById(idCategorie).style.display = "none";
-  document.getElementById("titlu").style.display = "none";
+function hideCategory(idCategory){
+  document.getElementById(idCategory).style.display = "none";
+  document.getElementById("title").style.display = "none";
 }
-ascundeVieti() ;
-function arataVieti() {
-  document.getElementById("Vieti").style.display = "block";
-  document.getElementById("categorieAleasa").style.display = "block";
+hideLifes() ;
+function showLifes() {
+  document.getElementById("lifes").style.display = "block";
+  document.getElementById("categoryChosen").style.display = "block";
 }
-function incepeJoc() {
-    ascundeJoc() ;
-     $('#categorie').append(`
-        <h5 id = "titlu">Alege categoria : </h5>
+function startGame() {
+    hideGame() ;
+     $('#category').append(`
+        <h5 id = "title">Chose category : </h5>
        `)
-   for ( var i = 0 ; i < categorii.length ; i++) {
-     $('#categorie').append(`
-       <button id = "` + i + `" onclick = "return alegeCuv(`+ i +`)"> `+ categorii[i] +`</button>
+   for ( var i = 0 ; i < categories.length ; i++) {
+     $('#category').append(`
+       <button id = "` + i + `" onclick = "return choseWord(`+ i +`)"> `+ categories[i] +`</button>
        `)
    }
    return false ;
 }
 var underScore = [] ;
-var cuvant ;
-var vieti = 5 ;
-function alegeCuv(idCuv) {
-  if ( idCuv == 0 ) {
-    var random = Math.floor(Math.random() * animale.length) ;
-    cuvant = animale[random] ;
-    ascundeCategorie(idCuv + 1 ) ;
+var word ;
+var lifes = 5 ;
+function choseWord(idWord) {
+  if ( idWord == 0 ) {
+    var random = Math.floor(Math.random() * animals.length) ;
+    word = animals[random] ;
+    hideCategory(idWord + 1 ) ;
   }
-  if ( idCuv == 1 ) {
-    var random = Math.floor(Math.random() * mancare.length) ;
-    cuvant = mancare[random] ;
-    ascundeCategorie(idCuv - 1 ) ;
+  if ( idWord == 1 ) {
+    var random = Math.floor(Math.random() * food.length) ;
+    word = food[random] ;
+    hideCategory(idWord - 1 ) ;
   }
-  var generezUnderScore = () => {
-     for ( var i = 0 ; i < cuvant.length ; i++) {
+  var generateUnderScore = () => {
+     for ( var i = 0 ; i < word.length ; i++) {
        underScore.push('_') ;
      }
      return underScore ;
   }
-  document.getElementById('cuvantAles').innerHTML = generezUnderScore() ;
+  document.getElementById('wordChosen').innerHTML = generateUnderScore() ;
   for ( var i = 65 ; i <= 90 ; i++ ) {
-      $('#litere').append(`
-        <button id = "`+ i +`" onclick = "return corect(`+ i +`)">`+String.fromCharCode(i)+`</button>
+      $('#letters').append(`
+        <button id = "`+ i +`" onclick = "return correct(`+ i +`)">`+String.fromCharCode(i)+`</button>
         `)
   }
-  $('#nrVieti').append(`
-    `+ vieti +`
+  $('#numberLifes').append(`
+    `+ lifes +`
     `)
-    arataVieti() ;
+    showLifes() ;
 }
-var i = 0 , numarCorecte = 0;
-function corect(idButon) {
- ascundeLitera(idButon) ;
- var verific = document.getElementById(idButon).innerText;
- var exista = 0  ;
- if( cuvant.indexOf(verific , i ) > -1 ) {
-    var pozitie = cuvant.indexOf(verific , i );
+var i = 0 , numberOfCorrect = 0;
+function correct(idButton) {
+ hideLetter(idButton) ;
+ var check = document.getElementById(idButton).innerText;
+ var exist = 0  ;
+ if( word.indexOf(check , i ) > -1 ) {
+    var position = word.indexOf(check , i );
     ++i ;
-    underScore.splice( pozitie , 1 , verific) ;
-    document.getElementById('cuvantAles').innerHTML = underScore  ;
-    while ( cuvant.indexOf(verific, i) > -1 ) {
-      pozitie = cuvant.indexOf(verific , i );
-      underScore.splice( pozitie , 1 , verific) ;
-      document.getElementById('cuvantAles').innerHTML = underScore ;
+    underScore.splice( position , 1 , check) ;
+    document.getElementById('wordChosen').innerHTML = underScore  ;
+    while ( word.indexOf(check, i) > -1 ) {
+      position = word.indexOf(check , i );
+      underScore.splice( position , 1 , check) ;
+      document.getElementById('wordChosen').innerHTML = underScore ;
       ++i ;
     }
     i = 0 ;
-    exista = 1 ;
+    exist = 1 ;
  }
- if ( exista == 0 ) {
+ if ( exist == 0 ) {
    gresit() ;
  }
  if ( underScore.indexOf("_") == -1) {
-     finalJoc() ;
+     finalGame() ;
  }
 }
 function gresit() {
-  vieti = vieti - 1 ;
-  if ( vieti == 0 ) {
+  lifes = lifes - 1 ;
+  if ( lifes == 0 ) {
     location.reload() ;
   }
-  document.getElementById('nrVieti').innerText = vieti ;
+  document.getElementById('numberLifes').innerText = lifes ;
 }
-function finalJoc()  {
-  document.getElementById('litere').style.display = "none" ;
-  document.getElementById('categorie').style.display = "none" ;
-  ascundeVieti() ;
-  document.getElementById('finalJoc').style.display = "block" ;
+function finalGame()  {
+  document.getElementById('letters').style.display = "none" ;
+  document.getElementById('category').style.display = "none" ;
+  hideLifes() ;
+  document.getElementById('finalGame').style.display = "block" ;
 
 }
